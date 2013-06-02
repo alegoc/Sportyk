9391dab1c08d9840620475ea99d05eed3de71127
aegea <alegoc.92@gmail.com>
2013-06-02 15:43:47 +0200

    First commit

---
+++ app/controllers/alberto_controller.rb
@@ -0,0 +1,11 @@
+class AlbertoController < ApplicationController
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prueba1::Application.config.secret_token = '6afbe618dc0b37ed53cf4afa30e9a381a0b6bafa2ae96b9a9dc8825c743125221d97bfad374ff6a0ee11ac7639ab0bd9c4f03a4b59246b2b94596d6a5431a643'
